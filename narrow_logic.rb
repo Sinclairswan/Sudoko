c47cea59914ae34114650229d11efe5faad5b691
Joanna Jackson & Joseph Kim & Sinclair Swan & Katie McNeil <pair+jo-hackson.josekim.sinclairswan.thekatiemcneil@devbootcamp.com>
2017-06-23 17:51:31 -0500

    finished narrow_logic

--- narrow_logic.rb
+++ narrow_logic.rb
@@ -2,45 +2,66 @@ require_relative 'search_row'
 require_relative 'search_column'
 require_relative 'search_block'
 require_relative 'available_rows_columns'
+require 'pry'
+
+board =
+    [["-","6","-","3","2","5","4","8","7"],
+     ["3","4","2","8","6","7","1","5","9"],
+     ["5","8","7","9","1","4","6","3","2"],
+     ["2","9","5","1","4","6","8","7","3"],
+     ["4","1","3","7","8","2","9","6","5"],
+     ["6","7","8","5","3","9","2","4","1"],
+     ["1","5","4","2","7","8","3","9","6"],
+     ["7","2","6","4","9","3","5","1","8"],
+     ["8","3","9","6","5","1","-","2","-"]]
+
 
 def narrow_logic(board)
-  flag = false
-  block_origins = {0=>[0,0],1=>[0,3],2=>[0,6],3=>[3,0],4=>[3,3],5=>[3,6],6=>[6,0],7=>[6,3],8=>[6,6] }
 
-  [1..9].times do |number|
-    [0..8].times do |block_number|
+  flag = false
+  block_origins = {1=>[0,0],2=>[0,3],3=>[0,6],4=>[3,0],5=>[3,3],6=>[3,6],7=>[6,0],8=>[6,3],9=>[6,6] }
+
+  9.times do |number|
+    number += 1
+    9.times do |block_number|
+      block_number += 1
       row, column = block_origins[block_number]
-      numbers_in_block = search_block(board, row, column) # all numbers present in block
-      if !numbers_in_block.include?(number)
+      numbers_in_block = search_block(board, row, column)
+      if !numbers_in_block.include?(number.to_s)
         flag_in_row = 0
         flag_in_col = 0
         remember_row = nil
         remember_column = nil
-        available_rows, available_columns = available_rows_columns(row, column) # find all available rows and columns in block
+        available_rows, available_columns = available_rows_columns(row, column)
         available_rows.each do |block_row|
-        values_in_row = search_row(board, block_row) # return values of that row
-          if !values_in_row.include?(number)
+        values_in_row = search_row(board, block_row)
+          if !values_in_row.include?(number.to_s)
             remember_row = block_row
             flag_in_row += 1
           end
         end
         if flag_in_row == 1
+          values_in_col = ''
           available_columns.each do |block_column|
-          values_in_column = search_column(board, block_column)
-            if !values_in_column.include?(number)
+          values_in_col = search_column(board, block_column)
+            if !values_in_col.include?(number.to_s)
               remember_column = block_column
               flag_in_col += 1
             end
           end
-          if flag_in_col == 1
-            board[remember_row][remember_column] = number
+          if flag_in_col == 1 && (board[remember_row][remember_column] == "-")
+            board[remember_row][remember_column] = number.to_s
+            flag = true
           end
+
         end
       end
     end
   end
   flag
 end
+
+narrow_logic(board)
 
 ## set flag = false
   ## for each number [1..9]
