cc21fdd4a79bbb2ef2a4adf5e27edb5a18466b23
Joanna Jackson & Joseph Kim & Sinclair Swan & Katie McNeil <pair+jo-hackson.josekim.sinclairswan.thekatiemcneil@devbootcamp.com>
2017-06-23 14:36:47 -0500

    Write search column method

--- search_row.rb
+++ search_row.rb
@@ -1,14 +1,5 @@
 def search_row(board,row_index)
   numbers_in_row = []
-  board[row_index].each_index do |index|
-    numbers_in_row << board[row_index][index] if board[row_index][index] != "-"
-  end
-  numbers_in_row
-end  ## returns array of what numbers are on a row
-
-
-def search_row(board,row_index)
-  numbers_in_row = []
   board[row_index].each do |columns_value|
     numbers_in_row << columns_value if columns_value != "-"
   end
